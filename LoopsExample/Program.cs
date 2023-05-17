2abe8c27d2a63bc58d04e18f07b1d9b675abb7c2
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-17 08:35:12 -0500

    Examples created

---
+++ LoopsExample/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace LoopsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //While loop
+            Console.WriteLine("WHILE LOOP:");
+            var cont = "yes";
+            while (cont == "yes")
+            {
+                Console.WriteLine("Continue? (Type 'yes' or 'no')");
+                cont = Console.ReadLine();
+            }
+
+            Console.WriteLine("Outside of the loop.");
+            Console.WriteLine("Press enter.");
+            Console.ReadKey();
+
+
+            //Do While loop
+            Console.WriteLine("DO-WHILE LOOP:");
+            var num = 1;
+
+            do
+            {
+                Console.WriteLine(num);
+                num++;
+            } while (num <= 5);
+
+            Console.WriteLine("Outside of the loop");
+            Console.WriteLine("Press enter.");
+            Console.ReadKey();
+
+
+            //For loop
+            Console.WriteLine("FOR LOOP:");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} is the lap we are on");
+            }
+            Console.WriteLine("Outside of the loop");
+            Console.WriteLine("Press enter.");
+            Console.ReadKey();
+
+            //Foreach loop
+            Console.WriteLine("FOREACH LOOP:");
+            var numbers = new int[] { 5, 8, 10, 44, 500, 6 };
+
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Outside of the loop");
+        }
+    }
+}
